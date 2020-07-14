28cbf4cadf1936721d3e0d665687af307dcfcb71
Research-shiva-official <68280091+Research-shiva-official@users.noreply.github.com>
2020-07-14 20:44:57 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,74 +1,51 @@
-"use strict";
-
-function getbase64(file,encoded){
-
-    if(encoded){
-     const toBase64 = file => new Promise((resolve, reject) => {
-         const reader = new FileReader();
-         reader.readAsDataURL(file);
-         reader.onload = () => {
-             let encoded = reader.result.toString().replace(/^data:(.*,)?/, '');
-             if ((encoded.length % 4) > 0) {
-               encoded += '='.repeat(4 - (encoded.length % 4));
-             }
-             resolve(encoded);
-           };
-           reader.onerror = error => reject(error);
-         });
-      
-     
-     
-         async function Main() {
-           
-           
-           const result = await toBase64(file).catch(e => Error(e));
-           if(result instanceof Error) {
-            
-              console.log('Error: ', result.message);
-              return;
-           }else{
-           
-           return result;
-            
-           }
-           
+function research(){
+  
+  const toBase64 = file => new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => {
+        let encoded = reader.result.toString().replace(/^data:(.*,)?/, '');
+        if ((encoded.length % 4) > 0) {
+          encoded += '='.repeat(4 - (encoded.length % 4));
         }
-        Main();
-    }else{
-     const toBase64 = file => new Promise((resolve, reject) => {
-         const reader = new FileReader();
-         reader.readAsDataURL(file);
-         reader.onload = () => {
-             let encoded = reader.result.toString().replace(/^data:(.*,)?/, '');
-             if ((encoded.length % 4) > 0) {
-               encoded += '='.repeat(4 - (encoded.length % 4));
-             }
-             resolve(reader.result.toString());
-           };
-           reader.onerror = error => reject(error);
-         });
-      
-     
-     
-         async function Main() {
-           
-           
-           const result = await toBase64(file).catch(e => Error(e));
-           if(result instanceof Error) {
-            
-              console.log('Error: ', result.message);
-              return;
-           }else{
-           
-           return result;
-            
-           }
-           
-        }
-        Main();
-    }
-     
-     
-     }
+        resolve(encoded);
+      };
+      reader.onerror = error => reject(error);
+    });
 
-var research=new getbase64();
+    const toBase644 = file => new Promise((resolve, reject) => {
+      const readerr = new FileReader();
+      readerr.readAsDataURL(file);
+      readerr.onload = () => {
+          let encodedd = reader.result.toString().replace(/^data:(.*,)?/, '');
+          if ((encodedd.length % 4) > 0) {
+            encodedd += '='.repeat(4 - (encodedd.length % 4));
+          }
+          resolve(encodedd);
+        };
+        readerr.onerror = error => reject(error);
+      });
+
+    
+    async function Main(file,encode) {
+           
+           
+      if(encode==true){
+        const result = await toBase64(file).catch(e => Error(e));
+      }else if(encode==false){
+        const result = await toBase644(file).catch(e => Error(e));
+      }
+      if(result instanceof Error) {
+       
+         console.error('Error: ', result.message);
+         return;
+      }else{
+      
+      return result;
+       
+      }
+      
+   }
+}
+
+var Research =new research;
